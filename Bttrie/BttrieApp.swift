5fc773423cb98cda6d3e27e87da2ad589d671487
CozyBrian <dragonbrian33@gmail.com>
2024-04-25 01:38:31 +0000

    Initial Commit

---
+++ Bttrie/BttrieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BttrieApp.swift
+//  Bttrie
+//
+//  Created by Brian Newton on 25/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct BttrieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
